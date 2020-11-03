d482202308270d290eb6f0966a6efdb0ad8db8b0
yolossn <nssvlr@gmail.com>
2020-11-03 21:53:28 +0530

    value changes

--- app/tasks.py
+++ app/tasks.py
@@ -3,9 +3,9 @@ import time
 import random
 
 
-@celery.task(name="report")
+@celery.task(name="report", acks_late=True)
 def report():
     print("Generating report")
-    time_span = random.randint(2,7)
+    time_span = random.randint(2,120)
     time.sleep(time_span)
     return {"state":"completed"}
