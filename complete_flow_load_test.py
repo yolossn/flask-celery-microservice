4af6adf7ae962c3254127c18e338a149b80ffef5
yolossn <nssvlr@gmail.com>
2021-03-10 15:45:17 +0530

    make complete flow test async

--- complete_flow_load_test.py
+++ complete_flow_load_test.py
@@ -1,32 +1,51 @@
+import gevent
+import requests
+import time
+import locust
 from locust import HttpUser, task, constant
 
+def async_success(name, start_time, resp):
+    locust.events.request_success.fire(
+        request_type=resp.request.method,
+        name=name,
+        response_time=int((time.monotonic() - start_time) * 1000),
+        response_length=len(resp.content),
+    )
+
+def async_failure(name, start_time, resp, message):
+    locust.events.request_failure.fire(
+        request_type=resp.request.method,
+        name=name,
+        response_time=int((time.monotonic() - start_time) * 1000),
+        exception=Exception(message),
+    )
 
 class reportService(HttpUser):
 
     wait_time = constant(1)
-    
-    def generate_report(self):
-        resp = self.client.post("report")
-        if resp.status_code != 200:
-            print("Error generating report")
-        return resp
-    
-    def check_status(self,id):
-        while(True):
-            print(id)
-            resp = self.client.get(f"report/{id}")
-            resp = resp.json()
-            print(resp)
-            if "result" in resp.keys():
-                if resp["result"]!= None:
-                    if resp["result"]["state"] == "completed":
-                        return resp
 
-    
+    def _do_async_thing_handler(self, timeout=600):
+        post_resp = requests.post(self.host + 'report')
+        if not post_resp.status_code == 200:
+            return
+        id = post_resp.json()['report_id']
+        print(id)
+
+        # Now poll for an ACTIVE status
+        start_time = time.monotonic()
+        end_time = start_time + timeout
+        while time.monotonic() < end_time:
+            r = requests.get(self.host + 'report/' + id)
+            if r.status_code == 200 and r.json()['result'] != None:
+                async_success('POST /report/ID - async', start_time, post_resp)
+                return
+
+            # IMPORTANT: Sleep must be monkey-patched by gevent (typical), or else
+            # use gevent.sleep to avoid blocking the world.
+            time.sleep(1)
+        async_failure('POST /report/ID - async', start_time, post_resp,
+                      'Failed - timed out after %s seconds' % timeout)
+
     @task
-    def complete_flow(self):
-        resp = self.generate_report()
-        resp = resp.json()
-        output = self.check_status(resp["report_id"])
-        print("done")
-    
+    def do_async_thing(self):
+        gevent.spawn(self._do_async_thing_handler)
